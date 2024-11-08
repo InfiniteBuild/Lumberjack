9081024c3c89c8c2581fee5095e0b2148e0e54e2
Jared Shipley <jared.shipley@siemens.com>
2024-11-08 07:49:14 +0100

    fix package deploy when "dev" is disabled

--- CM/GenerateNugetPackage.bat
+++ CM/GenerateNugetPackage.bat
@@ -50,7 +50,7 @@ echo ^</package^> >> %nugetDir%\interface.nuspec
 echo Generate Nuget package
 %nugetexe% pack %nugetDir%\interface.nuspec -OutputDirectory %nugetDir%
 
-%nugetexe% source |find "dev"
+%nugetexe% source |find "dev [Enabled]"
 if "%ERRORLEVEL%"=="0" %nugetexe% push %nugetDir%\*.nupkg -Source Dev
 
 :Done
