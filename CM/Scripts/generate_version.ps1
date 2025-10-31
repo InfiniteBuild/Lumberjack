b8437b6aac0292f843b3b982830d9d7fb87070a7
Jared Shipley <jared.shipley@siemens.com>
2025-10-31 10:06:48 +0100

    update generate_version.ps1

--- CM/Scripts/generate_version.ps1
+++ CM/Scripts/generate_version.ps1
@@ -94,13 +94,18 @@ Write-Host "Generated/Final Version: $env:VERSION"
 $env:VERSION | Out-File -FilePath "version.txt"
 $env:BuildNumber | Out-File -FilePath "buildnumber.txt"
 
-$variables = @{
+$variablesToExport = @(
 	"STATIC_VERSION=$env:STATIC_VERSION"
 	"CI_BUILD_NUMBER=$env:BuildNumber"
 	"CI_BUILD_VERSION=$env:VERSION"
-}
+)
 
-$variables | Set-Content -Path variables.env -Encoding ASCII -Force
+$variablesToExport | Set-Content -Path variables.env -Encoding ASCII -Force
+
+# Debugging check
+Write-Host "--- Generated variables.env Content ---"
+Get-Content -Path variables.env -Raw 
+Write-Host "---------------------------------------"
 
 # -------------------------------------------------------------------
 # STEP 3: Update CI/CD Variables (Only for Regular Branch Builds)
