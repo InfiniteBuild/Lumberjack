fc98100fe00a8720fa44f5b707b373cd2dcf0868
Jared Shipley <jared.shipley@siemens.com>
2025-11-01 09:46:13 +0100

    rework build system

--- CM/PublishProjects.bat
+++ CM/PublishProjects.bat
@@ -19,19 +19,6 @@ robocopy /e /s Common\Lumberjack48\bin\Debug %pubDebDir%\Lumberjack48
 dotnet build Common\Lumberjack48\Lumberjack48.csproj -p:Configuration=Release;Platform=AnyCPU -t:Rebuild
 robocopy /e /s Common\Lumberjack48\bin\Release %pubRelDir%\Lumberjack48
 
-mkdir %nugetDir% >NUL
-mkdir %nugetDir%\Data >NUL
-copy /y %rootDir%\License %nugetDir%\Data\License.txt
-copy /y %rootDir%\ReadMe.md %nugetDir%\Data\ReadMe.md
-copy /y %rootDir%\Resources\Lumberjack.png %nugetDir%\Data\Lumberjack.png
-
-echo.
-echo Retrieve Nuget package dependency info
-
-REM for readability, set the list in a variable (list is comma delimited)
-set csprojList=Common\LumberjackLib\LumberjackLib.csproj
-powershell %rootDir%\cm\scripts\generate_dependencies.ps1 -csprojFiles %csprojList% -outputDir %nugetDir%\Data
-
 popd
 
 :Done
--- CM/SetVariables.bat
+++ CM/SetVariables.bat
@@ -7,13 +7,20 @@ REM only set the variables once
 if not "%setvarScriptDir%"=="" goto Done
 
 :Force
+set scripterror=false
+
 set setvarScriptDir=%~dp0
 echo setvarScriptDir: %setvarScriptDir%
 
 set rootDir=%setvarScriptDir%..
 echo rootDir: %rootDir%
 
-set publishDir=%rootDir%\Publish
+set binaryDir=%rootDir%\binaries
+set buildDir=%binaryDir%\build
+set buildDebugDir=%buildDir%\Debug
+set buildReleaseDir=%buildDir%\Release
+
+set publishDir=%binaryDir%\Publish
 echo publishDir: %publishDir%
 
 set pubRelDir=%publishdir%\Release
@@ -22,10 +29,10 @@ echo pubRelDir: %pubRelDir%
 set pubDebDir=%publishdir%\Debug
 echo pubDebDir: %pubDebDir%
 
-set nugetDir=%PublishDir%\Nuget
+set nugetDir=%binaryDir%\Nuget
 echo nugetDir: %nugetDir%
 
-set zipDir=%PublishDir%\zip
+set zipDir=%binaryDir%\zip
 echo zipDir: %zipDir%
 
 for /f "tokens=1,2,3* delims=<>" %%i in (%rootDir%\CM\version\assemblyversion.props) do if "%%j"=="FileVersion" set version=%%k
